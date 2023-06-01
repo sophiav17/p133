17d651c7f0a629094f261d50a322e77f2caa9cb3
Sophia <99284300+sophiav17@users.noreply.github.com>
2023-06-01 15:59:15 +0100

    Add files via upload

---
+++ teddy.js
@@ -0,0 +1,43 @@
+img = ""
+status = ""
+objects = []
+
+function preload()
+{
+    img = loadImage("20230526_120357.jpg");
+}
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+function draw()
+{
+    if(status != "")
+    {
+        for(i = 0; i > objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y)
+        }
+    }
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
